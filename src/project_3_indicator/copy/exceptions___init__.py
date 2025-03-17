6eeff2348a7e2bb7b89d47a6061aab9b54048149
javierdomcal <javier.dom.cal@gmail.com>
2025-03-17 14:42:48 +0100

    updating changes

--- src/project_3_indicator/copy/exceptions___init__.py
+++
@@ -1,6 +0,0 @@
-"""
-Custom exceptions package initialization.
-"""
-# Import your custom exceptions here
-__all__ = []  # Add your exception classes here
-
--- src/project_3_indicator/copy/registry___init__.py
+++
@@ -1,6 +0,0 @@
-"""
-Registry package for managing calculation registration and tracking.
-"""
-from .registry import Registry
-
-__all__ = ["Registry"]
